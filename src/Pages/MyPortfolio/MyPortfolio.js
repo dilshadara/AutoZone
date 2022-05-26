8a050d5d310fcbef712685beffe9263cc6711348
ADUNSW\z3530200 <d.ara@blackdog.org.au>
2022-05-27 01:44:14 +1000

    portfolio done

--- src/Pages/MyPortfolio/MyPortfolio.js
+++ src/Pages/MyPortfolio/MyPortfolio.js
@@ -3,7 +3,23 @@ import React from 'react';
 const MyPortfolio = () => {
     return (
         <div className='h-screen'>
-            My Portfolio
+        <div className='flex justify-center mt-10 mb-10'>
+            <div class="card w-4/6 bg-base-100 shadow-xl">
+  <div class="card-body">
+    <h2 class="card-title">Dilshad Ara</h2>
+    <p><b>Email:</b> dolacsdu@gmail.com</p>
+    <p><b>Education:</b> Computer Science, Dhaka University</p>
+    <p><b>Skill:</b> C#.Net, MVC, HTML, CSS, bootstrap, tailwind, React</p>
+    <p><b>Live site links:</b> </p>
+    <ul>
+            <li className='cursor-pointer '>https://manufacturer-website-919e9.web.app/</li>
+            <li className='cursor-pointer'>https://warehouse-management-7a76b.firebaseapp.com/</li>
+            <li className='cursor-pointer'>https://delicate-crisp-045979.netlify.app</li>
+        </ul>
+    
+  </div>
+</div>
+        </div>
         </div>
     );
 };
